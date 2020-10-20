4f95d669463c1189853af479b0a601f8c6adc6a4
Willian <reiswn@gmail.com>
2020-10-20 19:14:06 -0300

    redirect & url_for

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/redirect')
+def redirect_url():
+    #return redirect('http://localhost:5000/')
+    return redirect(url_for('hello_world'))
 
 app.run(debug=True)
