8c429898eb964c9b3f21c4a46db2c0a5d6b7dd1b
Willian <reiswn@gmail.com>
2020-10-20 20:47:07 -0300

    flask template

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,10 @@
 from . import home
+from flask.templating import render_template
 
 @home.route('/home')
-def home_page():
-    return 'index page'
+@home.route('/home/<name>')
+def home_page(name=None):
+    page = "Home Page"
+    numbers = [1,2,4,8,16,32,64]
+
+    return render_template('home.html', page_var=page, numbers_var=numbers, name=name)
